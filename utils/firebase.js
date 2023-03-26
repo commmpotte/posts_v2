20c6b07b06aefd1f87133b197c9087f9c32bb5e6
commmpotte <kisel.igor.v@yandex.ru>
2023-03-26 11:26:12 +0300

    api

--- utils/firebase.js
+++ utils/firebase.js
@@ -13,12 +13,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-   apiKey: "AIzaSyBxx55ttnQMNWAEUDvMdUHlxMXKaJ3iwsU",
-       authDomain: "posts-app-8b3bf.firebaseapp.com",
-       projectId: "posts-app-8b3bf",
-       storageBucket: "posts-app-8b3bf.appspot.com",
-       messagingSenderId: "500789695539",
-       appId: "1:500789695539:web:1221fe3b81a44968797f46"
+ apiKey: process.env.NEXT_PUBLIC_API_KEY,
+     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+     appId: process.env.NEXT_PUBLIC_MESSAGING_APP_ID
 };
 
 // Initialize Firebase
